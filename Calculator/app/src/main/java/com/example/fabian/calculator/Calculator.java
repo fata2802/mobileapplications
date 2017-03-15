3e8e9957b487511aab7fbe7ce89929e93d7aa587
Fabian Henger <fabian.henger@student.tugraz.at>
2017-03-15 15:37:23 +0100

    Input tests compoleted

--- Calculator/app/src/main/java/com/example/fabian/calculator/Calculator.java
+++ Calculator/app/src/main/java/com/example/fabian/calculator/Calculator.java
@@ -10,48 +10,16 @@ import java.util.ArrayList;
 
 public class Calculator extends Activity implements View.OnClickListener {
 
-    ArrayList<Button> numberButtons;
-
     TextView numberView;
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
-    Button button5;
-    Button button6;
-    Button button7;
-    Button button8;
-    Button button9;
-    Button button0;
-    Button buttonC;
-    Button buttonEqual;
-    Button buttonPlus;
-    Button buttonMinus;
-    Button buttonMultiply;
-    Button buttonDivide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
+        setNumberButtonListener();
+
         numberView = (TextView) findViewById(R.id.numberView);
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button4);
-        button5 = (Button) findViewById(R.id.button5);
-        button6 = (Button) findViewById(R.id.button6);
-        button7 = (Button) findViewById(R.id.button7);
-        button8 = (Button) findViewById(R.id.button8);
-        button9 = (Button) findViewById(R.id.button9);
-        button0 = (Button) findViewById(R.id.button0);
-        buttonC = (Button) findViewById(R.id.buttonC);
-        buttonEqual = (Button) findViewById(R.id.buttonEqual);
-        buttonPlus = (Button) findViewById(R.id.buttonPlus);
-        buttonMinus = (Button) findViewById(R.id.buttonMinus);
-        buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
-        buttonDivide = (Button) findViewById(R.id.buttonDivide);
     }
 
 
@@ -88,8 +56,20 @@ public class Calculator extends Activity implements View.OnClickListener {
             int id = getResources().getIdentifier(buttonName, "id", R.class.getPackage().getName());
             Button button = (Button) findViewById(id);
             button.setOnClickListener(this);
-
-            numberButtons.add(button);
         }
+
+        Button tempButton;
+        tempButton = (Button) findViewById(R.id.buttonC);
+        tempButton.setOnClickListener(this);
+        tempButton = (Button) findViewById(R.id.buttonEqual);
+        tempButton.setOnClickListener(this);
+        tempButton = (Button) findViewById(R.id.buttonPlus);
+        tempButton.setOnClickListener(this);
+        tempButton = (Button) findViewById(R.id.buttonMinus);
+        tempButton.setOnClickListener(this);
+        tempButton = (Button) findViewById(R.id.buttonMultiply);
+        tempButton.setOnClickListener(this);
+        tempButton = (Button) findViewById(R.id.buttonDivide);
+        tempButton.setOnClickListener(this);
     }
 }
